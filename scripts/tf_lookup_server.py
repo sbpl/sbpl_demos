5736fb2ef662d0b3cdc477dcc0ba21b8831ce2da
Taehyung Kim <cyberphysicalrobotics@gmail.com>
2017-01-18 19:47:23 -0500

    implemented tf_lookup_server.

--- scripts/tf_lookup_server.py
+++ scripts/tf_lookup_server.py
@@ -1,20 +1,43 @@
+#!/usr/bin/env python
 import rospy
 import tf
 # from tf_lookup_server.srv import parent_frame,child_frame
 from std_msgs.msg import String,Float64
-from tf_lookup_server.srv import *
-def handle_tf_lookup_server(req):
-	print req.parent_frame
-	print req.child_frame
+from geometry_msgs.msg import Pose, Point, Quaternion
+from sbpl_demos.srv import *
 
+class TfLookupServer:
+	def __init__(self):
+		self.listener = tf.TransformListener()
+		return
+	def handle_tf_lookup_server(self, req):
+		now = rospy.Time.now()
+		self.listener.waitForTransform(req.parent_frame, req.child_frame, now, rospy.Duration(req.max_timeout))
+		(trans,rot) = self.listener.lookupTransform(req.parent_frame, req.child_frame, now)
+
+		pose = Pose()
+		pose.position.x = trans[0]
+		pose.position.y = trans[1]
+		pose.position.z = trans[2]
+
+		pose.orientation.x = rot[0]
+		pose.orientation.y = rot[1]
+		pose.orientation.z = rot[2]
+		pose.orientation.w = rot[3]
+		
+		return TfLookupResponse(pose)
+
+	def run(self):
+		robot = rospy.get_param("robot", "default_robot")
+		service = rospy.Service(robot+'/tf_lookup_server', TfLookup, self.handle_tf_lookup_server)		
+		return
 
 if __name__ == '__main__':
-    rospy.init_node('tf_lookup_server')
-    service = rospy.Service('tf_lookup_server', sbpl_demos.srv.tf_lookup_server, handle_tf_lookup_server)
-    # service = rospy.Service('tf_lookup_server', parent_frame, handle_tf_lookup_server)
-    
-    rate=rospy.Rate(10)
+	rospy.init_node('tf_lookup_server')
+	srv = TfLookupServer()
+	srv.run()
+	rospy.spin()
 
-    while not rospy.is_shutdown():
-    	rate.sleep()
-
+	rate=rospy.Rate(10)
+	while not rospy.is_shutdown():
+		rate.sleep()
