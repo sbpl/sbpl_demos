3db1f20e8eac32e95ae71793ae334f59cc484415
abrinkmacmu <abrinkma@andrew.cmu.edu>
2017-01-24 18:24:27 -0500

    move_base test is working acceptably

--- scripts/pr2_move_base.py
+++ scripts/pr2_move_base.py
@@ -26,15 +26,16 @@ if __name__ == '__main__':
     geometry_msgs/PoseStamped base_position
 
     '''
+
     goal.target_pose.header.stamp = rospy.Time.now()
-    goal.target_pose.header.frame_id = 'odom_combined'
-    goal.target_pose.pose.position.x = 0.25
-    goal.target_pose.pose.position.y = 0.0
+    goal.target_pose.header.frame_id = 'map'
+    goal.target_pose.pose.position.x = .291
+    goal.target_pose.pose.position.y = -1.334
     goal.target_pose.pose.position.z = 0.0
     goal.target_pose.pose.orientation.x = 0
     goal.target_pose.pose.orientation.y = 0
-    goal.target_pose.pose.orientation.z = 0
-    goal.target_pose.pose.orientation.w = 1
+    goal.target_pose.pose.orientation.z = -0.343
+    goal.target_pose.pose.orientation.w = 0.94
 
     client.send_goal(goal)
     if client.wait_for_result():
