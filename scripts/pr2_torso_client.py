945a7607627384e105e6d98a33706a33e3092130
abrinkmacmu <abrinkma@andrew.cmu.edu>
2017-01-24 11:31:56 -0500

    added pr2 startup launch

--- scripts/pr2_torso_client.py
+++ scripts/pr2_torso_client.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-
+import sys
 import roslib
 roslib.load_manifest('sbpl_demos')
 roslib.load_manifest('pr2_controllers_msgs')
@@ -9,17 +9,26 @@ import math
 
 import IPython
 #IPython.embed()
-from pr2_controllers_msgs.msg import SingleJointPositionAction, SingleJointPositionGoal
+from pr2_controllers_msgs.msg import *
+
+def state_cb(msg):
+    print msg.feedback.position
 
 if __name__ == '__main__':
     rospy.init_node('client')
     #IPython.embed()
-    client = actionlib.SimpleActionClient('torso_controller/position_joint_action', SingleJointPositionAction)
-    #sub = rospy.Subscriber("torso_controller/state", JointState, state_cb)
+    client = actionlib.SimpleActionClient('/torso_controller/position_joint_action', SingleJointPositionAction)
+    #sub = rospy.Subscriber("pr2/torso_controller/position_joint_action/feedback", SingleJointPositionFeedback, state_cb)
+    print("waiting for server...")
+    #pub = rospy.Publisher("/torso_controller/position_joint_action/goal", SingleJointPositionActionGoal, queue_size=1)
     client.wait_for_server()
+    #print("connected!")
+    #rospy.sleep(1)
 
     goal = SingleJointPositionGoal()
-    goal.position = 0 
+    goal.position = int(sys.argv[1]) 
     client.send_goal(goal)
+    #pub.publish(goal)
     rospy.sleep(2.0)
     print ('sent goal!')
+
