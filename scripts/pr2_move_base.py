470e628bc0d559091c21f613fdddd3b7bf0c6396
abrinkmacmu <abrinkma@andrew.cmu.edu>
2017-01-24 17:56:40 -0500

    added tf2 lookup cient and experimental move_base client

---
+++ scripts/pr2_move_base.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python
+import sys
+import roslib
+roslib.load_manifest('sbpl_demos')
+roslib.load_manifest('move_base_msgs')
+import rospy
+import actionlib
+import math
+
+import IPython
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+
+if __name__ == '__main__':
+    rospy.init_node('client')
+
+    client = actionlib.SimpleActionClient('pr2_move_base', MoveBaseAction)
+    print("waiting for server...")
+
+    client.wait_for_server()
+    #rospy.sleep(2)
+    goal = MoveBaseGoal()
+    '''
+    geometry_msgs/PoseStamped target_pose
+    ---
+    ---
+    geometry_msgs/PoseStamped base_position
+
+    '''
+    goal.target_pose.header.stamp = rospy.Time.now()
+    goal.target_pose.header.frame_id = 'odom_combined'
+    goal.target_pose.pose.position.x = 0.25
+    goal.target_pose.pose.position.y = 0.0
+    goal.target_pose.pose.position.z = 0.0
+    goal.target_pose.pose.orientation.x = 0
+    goal.target_pose.pose.orientation.y = 0
+    goal.target_pose.pose.orientation.z = 0
+    goal.target_pose.pose.orientation.w = 1
+
+    client.send_goal(goal)
+    if client.wait_for_result():
+        res = client.get_result()
+        print res
+        print "Moved to location successfully"
+    else:
+        print "Did not complete successfully"
+    print ('Done!')
+
---
+++ scripts/pr2_tf_lookup_client.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python
+import sys
+import roslib
+roslib.load_manifest('sbpl_demos')
+roslib.load_manifest('tf2_msgs')
+import rospy
+import actionlib
+import math
+
+import IPython
+from tf2_msgs.msg import LookupTransformAction, LookupTransformGoal
+
+if __name__ == '__main__':
+    rospy.init_node('client')
+
+    client = actionlib.SimpleActionClient('tf2_buffer_server', LookupTransformAction)
+    print("waiting for server...")
+
+    client.wait_for_server()
+
+    goal = LookupTransformGoal()
+    '''
+    #Simple API
+    string target_frame
+    string source_frame
+    time source_time
+    duration timeout
+
+    #Advanced API
+    time target_time
+    string fixed_frame
+
+    #Whether or not to use the advanced API
+    bool advanced
+
+    ---
+    geometry_msgs/TransformStamped transform
+    tf2_msgs/TF2Error error
+    ---
+    '''
+    goal.target_frame = sys.argv[1]
+    goal.source_frame = sys.argv[2]
+    #goal.source_time = rospy.Time.now()
+    goal.timeout = rospy.Time(1)
+    client.send_goal(goal)
+    if client.wait_for_result():
+        res = client.get_result()
+        print res.transform
+    else:
+        print "Did not complete successfully"
+    print ('Done!')
+
