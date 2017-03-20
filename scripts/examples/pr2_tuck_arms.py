710c41caa8571295497d44d3500623b7cc58a4bf
tk1 <tk1@andrew.cmu.edu>
2017-03-20 13:10:38 -0400

    added pr2_tuck_arms.

---
+++ scripts/examples/pr2_tuck_arms.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+import rospy
+from sbpl_demos import pr2_helpers
+
+if __name__ == "__main__":
+	rospy.init_node('pr2_grasp_test')
+	TuckArms = pr2_helpers.TuckArms()
+	TuckArms.TuckArms()
