552fc2bbfef92f2156c888f9a7475f0fbbd3b130
Sung Kyun Kim <kimsk@cs.cmu.edu>
2017-04-14 22:53:40 -0700

    Added script grasp_return in scripts/sbpl_demos to set parameters for ycb objects as rosparams

--- scripts/sbpl_demos/grasp_return.py
+++ scripts/sbpl_demos/grasp_return.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import sys
 import roslib
 import rospy
@@ -11,11 +13,36 @@ roslib.load_manifest('sbpl_demos')
 
 
 
-if __name__ == "main":
+if __name__ == "__main__":
 
-	fid = file('../data/grasp_database/grasp_database.yaml','r')
+	rospy.init_node('objects_to_grasps')
+	
+	fid = file('../../data/grasp_database/grasp_database.yaml')
+	print "hello"
+	try:
+		config = yaml.load(fid)
+	except yaml.YAMLError, exc:
+		print "Error in configuration file:", exc
 
-	yaml.load(fid)
+	str_namespace = '/grasps'
+	for key,value in config.items():
+		print "\nItem: "+str(key)
+		str_item = str_namespace+str('/')+str(key)
+		print str_item
+		print "Num_graps: "+str(len(config[key].items()))
+		rospy.set_param(str_item+'/num_grasps', len(config[key].items()))
 
+		for grasp_i_key, grasp_i_value in config[key].items():
+			grasp_index = str(grasp_i_key)[-1]
+			grasp_n = str_item+'/'+grasp_index
+			print "Grasp No: "+str(grasp_i_key)[-1]
+			print value[grasp_i_key]['pregrasp']
+			print value[grasp_i_key]['grasp']
+
+			rospy.set_param(grasp_n+'/pregrasp/rot_w_x_y_z', value[grasp_i_key]['pregrasp']['rotation'])
+			rospy.set_param(grasp_n+'/pregrasp/trans_x_y_z', value[grasp_i_key]['pregrasp']['translation'])
+
+			rospy.set_param(grasp_n+'/grasp/rot_w_x_y_z', value[grasp_i_key]['grasp']['rotation'])
+			rospy.set_param(grasp_n+'/grasp/trans_x_y_z', value[grasp_i_key]['grasp']['translation'])
 
 
