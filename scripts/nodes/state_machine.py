ff435d2d3bd18e806d624da9ecbea154853404d0
Sung Kyun Kim <kimsk@cs.cmu.edu>
2017-04-19 13:15:56 -0700

    redefined srv/StateMachine.srv for request_object, ROMAN_STATE, PR2_STATE

--- scripts/nodes/state_machine.py
+++ scripts/nodes/state_machine.py
@@ -4,28 +4,49 @@ import rospy
 
 from sbpl_demos.srv import StateMachine
 
-state = "WAIT"
+state = {'requested_object':'', 'ROMAN_STATE':'IDLE', 'PR2_STATE':'IDLE'}
+
+# StateMachine.srv
+#
+# string command
+# string request_key
+# string request_value
+# ---
+# bool result
+# string result_value
+
+# command: 'Get', 'Set'
+# request_key: 'requested_object', 'ROMAN_STATE', 'PR2_STATE'
+
+# key: 'requested_object'
+# value: '', '003_cracker_box', '006_mustard_bottle'
+#
+# key: 'ROMAN_STATE'
+# value: 'IDLE', 'MOVE_ARM', 'DONE'
+#
+# key: 'PR2_STATE'
+# value: 'IDLE', 'MOVE_ARM', 'MOVE_BASE', 'DONE'
 
 def state_machine(request):
+    global state
+
     if request.command == "Get":
-        global state
-        result = True
-        result_state = state
-        return result, result_state
-    
-    if request.command == "Set":
-        state = request.request_state
-        result_state = state
-        if result_state == request.request_state:
+        if request.request_key in state:
             result = True
-            return result, result_state
+            return (result, state[request.request_key])
         else:
             result = False
-            return result, result_state;
+            return (result, '')
+
+    if request.command == "Set":
+        if not (request.request_key in state):
+            rospy.logwarn("request_key '%s' is not in 'state' dictionary! Adding this as a new key...", request.request_key)
+        state[request.request_key] = request.request_value
+        result = True
+        return (result, state[request.request_key])
 
 if __name__ == "__main__":
     rospy.init_node('state_machine');
     service = rospy.Service('state_machine', StateMachine, state_machine)
-
-    # State 1: WAIT, State 2: ROMAN 3: PR2
+    rospy.loginfo("/state_machine is now running...")
     rospy.spin()
