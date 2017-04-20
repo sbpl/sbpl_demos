23de25eaa0fd67141e11f982c084dad386025cf6
Sung Kyun Kim <kimsk@cs.cmu.edu>
2017-04-19 19:52:42 -0700

    final --hopefully -- grasp database with 4 ycb objects and test scripts to get parameters

--- scripts/single_robot_demos/objects_to_grasps.py
+++ scripts/single_robot_demos/objects_to_grasps.py
@@ -26,7 +26,6 @@ def call_return_joint_states(joint_names):
             print "joint %s not found!"%joint_name
     return (resp.position, resp.velocity, resp.effort)
 
-
 #pretty-print list to string
 def pplist(list):
     return ' '.join(['%2.3f'%x for x in list])
@@ -41,16 +40,17 @@ if __name__ == "__main__":
     listener = tf.TransformListener()
     
     rate = rospy.Rate(10.0)
+
+    object_pose = PerchClient.getObjectPose("003_cracker_box")
+    # object_pose = PerchClient.getObjectPose("019_pitcher_base")
+    # object_pose = PerchClient.getObjectPose("010_potted_meat_can")
+    
+    # object_pose = PerchClient.getObjectPose("006_mustard_bottle")
+    
+    raw_input("object pose registered, hit enter to get first pose")
+    
     while not rospy.is_shutdown():
         
-        # object_pose = PerchClient.getObjectPose("003_cracker_box")
-        # object_pose = PerchClient.getObjectPose("019_pitcher_base")
-        object_pose = PerchClient.getObjectPose("006_mustard_bottle")
-        # object_pose = PerchClient.getObjectPose("010_potted_meat_can")
-
-
-        raw_input("object pose registered, hit enter to get first pose")
-
         try:
             (pose_base_to_wrist, quat_base_to_wrist) = listener.lookupTransform("base_footprint", "r_wrist_roll_link", rospy.Time(0))
         except (tf.LookupException):
@@ -109,45 +109,45 @@ if __name__ == "__main__":
         raw_input("hit enter to get next tf")
 
 
-        ####################################
-        ####################################
-        ####################################
+        # ####################################
+        # ####################################
+        # ####################################
         
-        try:
-            (pose_base_to_wrist, quat_base_to_wrist) = listener.lookupTransform("base_footprint", "r_wrist_roll_link", rospy.Time(0))
-        except (tf.LookupException):
-            print "lookup exception"
-            continue
-        except (tf.ConnectivityException):
-            print "connect exception"
-            continue
-        except (tf.ExtrapolationException):
-            print "extrap exception"
-            continue
+        # try:
+        #     (pose_base_to_wrist, quat_base_to_wrist) = listener.lookupTransform("base_footprint", "r_wrist_roll_link", rospy.Time(0))
+        # except (tf.LookupException):
+        #     print "lookup exception"
+        #     continue
+        # except (tf.ConnectivityException):
+        #     print "connect exception"
+        #     continue
+        # except (tf.ExtrapolationException):
+        #     print "extrap exception"
+        #     continue
 
-        T_b_w = listener.fromTranslationRotation(pose_base_to_wrist, quat_base_to_wrist)
-        T_b_o = listener.fromTranslationRotation(object_transf_trans, object_transf_rot)
+        # T_b_w = listener.fromTranslationRotation(pose_base_to_wrist, quat_base_to_wrist)
+        # T_b_o = listener.fromTranslationRotation(object_transf_trans, object_transf_rot)
 
         
 
-        # T_o_b = np.eye(4)
-        # T_o_b[:3, :3] = np.linalg.inv(T_b_o[:3,:3])
-        # T_o_b[:3, 3] = -np.dot(T_o_b[:3, :3],T_b_o[:3,3])
+        # # T_o_b = np.eye(4)
+        # # T_o_b[:3, :3] = np.linalg.inv(T_b_o[:3,:3])
+        # # T_o_b[:3, 3] = -np.dot(T_o_b[:3, :3],T_b_o[:3,3])
 
-        T_o_b = np.linalg.inv(T_b_o)
+        # T_o_b = np.linalg.inv(T_b_o)
 
-        print "transforming..." + str(T_o_b )+ " times "+ str(T_b_w)
+        # print "transforming..." + str(T_o_b )+ " times "+ str(T_b_w)
 
-        T_o_w = np.dot(T_o_b,T_b_w)
+        # T_o_w = np.dot(T_o_b,T_b_w)
 
-        #print tf.transformations.inverse_matrix(T_o_w[:3,:3])
+        # #print tf.transformations.inverse_matrix(T_o_w[:3,:3])
 
-        # print "Base to wrist: Pose: " + str(pose_base_to_wrist) + ", Quat:" + str(quat_base_to_wrist)
-        # print "Wrist Transform: " + str(T_b_w)
-        # print "Object Transform: " + str(T_b_o)
-        # print "Object to Wrist Transform: \n" + str(T_o_w)
-        print "quaternion: "+ str(tr.quaternion_from_matrix(T_o_w))
-        print "translation vector: "+ str(T_o_w[:3,3])
-        # print "Object pose" + str(object_pose)
+        # # print "Base to wrist: Pose: " + str(pose_base_to_wrist) + ", Quat:" + str(quat_base_to_wrist)
+        # # print "Wrist Transform: " + str(T_b_w)
+        # # print "Object Transform: " + str(T_b_o)
+        # # print "Object to Wrist Transform: \n" + str(T_o_w)
+        # print "quaternion: "+ str(tr.quaternion_from_matrix(T_o_w))
+        # print "translation vector: "+ str(T_o_w[:3,3])
+        # # print "Object pose" + str(object_pose)
 
-        raw_input("hit enter to get next tf")
+        # raw_input("hit enter to get next tf")
