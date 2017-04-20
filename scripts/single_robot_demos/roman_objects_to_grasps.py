b6c9e12ee295c0a7a4c64a4635930a9ee2888f14
Taehyung Kim <cyberphysicalrobotics@gmail.com>
2017-04-20 03:57:36 -0400

    Auto save grasp database and load it. Update without manual editing

--- scripts/single_robot_demos/roman_objects_to_grasps.py
+++ scripts/single_robot_demos/roman_objects_to_grasps.py
@@ -12,26 +12,56 @@ import numpy as np
 from sbpl_demos import perch_helpers
 from tf import transformations as tr
 import json
-
-def loadGraspDatabase( db_file):
+from rospkg import RosPack
+import os
+import shutil
+import time
+pkg = RosPack()
+db_file = os.path.join(pkg.get_path("sbpl_demos"), "data/grasp_database/roman_grasp_db.json")
+def graspDatabaseToJSON(db):
+    db = { key : [ { state : value.tolist() for state,value in grasp.iteritems() } for grasp in grasps ] for key, grasps in db.iteritems()}
+    return json.dumps(db, indent=4,sort_keys=True, separators=(',',': '))
+def loadGraspDatabase():
+    if not os.path.exists(db_file):
+        print "No db file detected. Creating a new one"
+        return dict()
     with open(db_file,'r') as f:
-        serialized = json.load(f)
-        self.db = { key : [ { state : np.array(value) for state,value in grasp.iteritems() } for grasp in grasps ] for key, grasps in serialized.iteritems()}
+        try:
+            serialized = json.load(f)
+        except:
+            print("Unable to deserialize json (creating new dict):", sys.exc_info()[0])
+            return dict()
+        try:
+            db = { key : [ { state : np.array(value) for state,value in grasp.iteritems() } for grasp in grasps ] for key, grasps in serialized.iteritems()}
+        except:
+            print("Unable to parse json (creating new dict):", sys.exc_info()[0])
+            return dict()
+        return db
+def save(db):
+    with open(db_file, 'w') as f:
+        f.write(graspDatabaseToJSON(db))
+def backup(db):
+    saveas = "/tmp/db.json.backup."+ time.strftime("%d.%m.%y.%H.%M.%S")
+    with open(saveas, 'w') as f:
+        f.write(graspDatabaseToJSON(db))
+        return saveas
 #print out the positions, velocities, and efforts of the right arm joints
 if __name__ == "__main__":
     rospy.init_node('objects_to_grasps')
-   # db = loadGraspDatabase("roman_grasp_db.json")
-    obj = "006_mustard_bottle"
     PerchClient = perch_helpers.PerchClient()
     listener = tf.TransformListener()
+    print "Loading Grasp Database..."
+    db = loadGraspDatabase()
+    print "OK!"
     while not rospy.is_shutdown():
+        obj = raw_input("input object id: ")
         object_pose = PerchClient.getObjectPose(obj)
-        for i in range(0,2):
-            raw_input("hit enter to capture grasping pose")
+        for i in range(0,1):
+            raw_input("hit enter to capture grasping pose for "+obj)
             listener.waitForTransform("limb_right_link7", "base_footprint", rospy.Time(),rospy.Duration(5))
 
             try:
-                (pose_base_to_wrist, quat_base_to_wrist) = listener.lookupTransform("limb_right_link7", "base_footprint", rospy.Time())
+                (pose_base_to_wrist, quat_base_to_wrist) = listener.lookupTransform("base_footprint", "limb_right_link7", rospy.Time())
             except (tf.LookupException):
                 print "lookup exception"
                 continue
@@ -47,21 +77,23 @@ if __name__ == "__main__":
 
             T_b_w = listener.fromTranslationRotation(pose_base_to_wrist, quat_base_to_wrist)
             T_b_o = listener.fromTranslationRotation(object_transf_trans, object_transf_rot)
-            import IPython
-            IPython.embed()
             T_o_b = np.linalg.inv(T_b_o)
 
-            print "transforming..." + str(T_o_b )+ " times "+ str(T_b_w)
 
             T_o_w = np.dot(T_o_b,T_b_w)
             T_w_o = np.linalg.inv(T_o_w)
-            test = np.dot(T_b_o, T_w_o)
-            print str(test)
-            print str(T_o_b)
+            test = np.dot(T_b_o, T_o_w)
+            if not np.allclose(test, T_b_w):
+                print "Expected:" + str(test) + " to be close to " + T_b_w
             quat = tr.quaternion_from_matrix(T_w_o)
             translation = T_w_o[:3,3]
-            entry = {obj : [{"grasp" : T_w_o.tolist()}]}
-            print "Enter this into database:", json.dumps(entry, indent=4, sort_keys=True, separators=(',',': '))
-            print "translation: " + str(translation)
-            print "quat: " + str(quat)
-
+            print "translation: " + str(translation) + " quat: " + str(quat)
+            option = raw_input("Save? (Y/N)")
+            print "Backing up to..." + backup(db)
+            if option == "Y" or option == "y":
+                if not(obj in db):
+                    db[obj]=list()
+                db[obj].append({"grasp" : T_w_o}) 
+            save(db)
+            print "Updated Database!"
+            
